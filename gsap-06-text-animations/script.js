8c956bac346816c9047c833e8aeab87a79baad4f
vilas chauvhan <vilas.chauvhan@seaflux.tech>
2024-08-31 13:05:25 +0530

    gsap text animation done

---
+++ gsap-06-text-animations/script.js
@@ -0,0 +1,36 @@
+function breakText() {
+  var text = document.querySelector("h1");
+  var h1Text = text.textContent;
+
+  var splitText = h1Text.split("");
+
+  var clutter = "";
+  var half = Math.floor(splitText.length / 2);
+
+  splitText.forEach(function (elem, index) {
+    if (index < half) {
+      clutter += `<span class="a">${elem}</span>`;
+    } else {
+      clutter += `<span class="b">${elem}</span>`;
+    }
+  });
+
+  text.innerHTML = clutter;
+}
+
+breakText();
+
+gsap.from("h1 .a", {
+  y: 50,
+  opacity: 0,
+  duration: 0.6,
+  delay: 0.5,
+  stagger: 0.15,
+});
+gsap.from("h1 .b", {
+  y: 50,
+  opacity: 0,
+  duration: 0.6,
+  delay: 0.5,
+  stagger: -0.15,
+});
